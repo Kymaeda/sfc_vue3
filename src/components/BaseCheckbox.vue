617d54636f2d5fadaca7e47696121f7279c67617
Kymaeda <kyo.m.720@gmail.com>
2022-07-30 11:22:08 +0900

    translant todo app

---
+++ src/components/BaseCheckbox.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex items-center">
+    <label>
+      <input
+        type="checkbox"
+        class="h-4 w-4 text-indigo-600 border-gray-300 rounded mr-2"
+        :checked="modelValue"
+        @change="onChange"
+      />
+      <slot>default</slot>
+    </label>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    modelValue: {
+      type: Boolean,
+      default: false,
+    },
+    label: {
+      type: String,
+    },
+  },
+  emits: ["update:modelValue"],
+  methods: {
+    onChange() {
+      this.$emit("update:modelValue", !this.modelValue);
+    },
+  },
+};
+</script>
