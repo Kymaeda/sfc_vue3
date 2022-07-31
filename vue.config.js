98118032a1f9a13990b68fd92cedb64241b748e7
Kymaeda <kyo.m.720@gmail.com>
2022-07-31 10:29:48 +0900

    customize vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,11 @@
 const { defineConfig } = require("@vue/cli-service");
 module.exports = defineConfig({
   transpileDependencies: true,
+  publicPath: process.env.NODE_ENV === "production" ? "/app" : "/",
+  pages: {
+    index: {
+      title: "Top Page",
+      entry: "src/main.js",
+    },
+  },
 });
