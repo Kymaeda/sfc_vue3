6e752df689b3183a4eb631129be068522d66f0dd
Kymaeda <kyo.m.720@gmail.com>
2022-08-10 08:56:17 +0900

    install vuex and setup store
    - install
    ```
    yarn add vuex@next --save
    ```
    cf. https://vuex.vuejs.org/ja/installation.html
    
    - setup
    cf. https://vuex.vuejs.org/ja/guide/

---
+++ src/store/index.js
@@ -0,0 +1,38 @@
+import { createStore } from "vuex";
+
+const store = createStore({
+  state() {
+    return {
+      tasks: [
+        {
+          id: 1,
+          description: "Buy food for the dog",
+          priority: false,
+          done: false,
+        },
+        {
+          id: 2,
+          description: "Pay the bills",
+          priority: true,
+          done: false,
+        },
+        {
+          id: 3,
+          description: "Buy some computer games",
+          priority: false,
+          done: false,
+        },
+        {
+          id: 4,
+          description: "Go to the gym",
+          priority: false,
+          done: false,
+        },
+      ],
+    };
+  },
+  getters: {},
+  mutations: {},
+});
+
+export default store;
