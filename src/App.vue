2243c528fee2a361a5c4b727caaa8395bfd0e36b
Kymaeda <kyo.m.720@gmail.com>
2022-08-14 18:11:22 +0900

    introduce projects

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,38 @@
 <template>
-  <div class="mb-4">
-    <AddTaskInput @added="taskAdded"></AddTaskInput>
-    <BaseCheckbox
-      class="mb-4 p-4 text-gray-600 text-sm font-weight-100"
-      v-model="onlyPending"
+  <div class="flex flex-col md:flex-row">
+    <div
+      class="w-full md:w-1/3 xl:w-1/5 mr-4 px-0 md:px-4 mb-4 h-full text-lg md:text-sm"
     >
-      <b>only wip</b>
-    </BaseCheckbox>
-  </div>
-  <div class="grid grid-cols-1 gap-4">
-    <TodoListItem
-      v-for="task in displayedTasks"
-      :task="task"
-      :key="task.id"
-      :done="task.done"
-      :priority="task.priority"
-      @update:done="updateTask(task, { done: $event })"
-      @update:priority="updateTask(task, { priority: $event })"
-    ></TodoListItem>
-    <SummaryLine class="mt-8" />
+      <ProjectList :projects="projects" />
+    </div>
+    <div class="w-full md:w-2/3 xl:w-4/5">
+      <div class="mb-4">
+        <AddTaskInput @added="taskAdded"></AddTaskInput>
+        <BaseCheckbox
+          class="mb-4 p-4 text-gray-600 text-sm font-weight-100"
+          v-model="onlyPending"
+        >
+          <b>only wip</b>
+        </BaseCheckbox>
+      </div>
+      <div class="grid grid-cols-1 gap-4">
+        <TodoListItem
+          v-for="task in displayedTasks"
+          :task="task"
+          :key="task.id"
+          :done="task.done"
+          :priority="task.priority"
+          @update:done="updateTask(task, { done: $event })"
+          @update:priority="updateTask(task, { priority: $event })"
+        ></TodoListItem>
+        <SummaryLine class="mt-8" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import ProjectList from "./components/ProjectList.vue";
 import TodoListItem from "./components/TodoListItem.vue";
 import AddTaskInput from "./components/AddTaskInput.vue";
 import BaseCheckbox from "./components/BaseCheckbox.vue";
@@ -35,13 +45,20 @@ export default {
     AddTaskInput,
     BaseCheckbox,
     SummaryLine,
+    ProjectList,
   },
   data() {
     return {};
   },
   computed: {
+    projects() {
+      return this.$store.state.projects;
+    },
     tasks() {
-      return this.$store.state.tasks;
+      const currentProject = this.$store.state.projects.find(
+        (project) => project.id === this.$store.state.currentProjectId
+      );
+      return currentProject?.tasks ?? [];
     },
     displayedTasks() {
       return [...this.tasks]
--- src/store/index.js
+++ src/store/index.js
@@ -4,6 +4,7 @@ const store = createStore({
   state() {
     return {
       onlyPending: false,
+      currentProjectId: 1,
       tasks: [
         {
           id: 1,
@@ -28,6 +29,68 @@ const store = createStore({
           description: "Go to the gym",
           priority: false,
           done: false,
+        },
+      ],
+      projects: [
+        {
+          id: 1,
+          name: "First Project",
+          tasks: [
+            {
+              id: 1,
+              description: "Buy food for the dog",
+              priority: false,
+              done: false,
+            },
+            {
+              id: 2,
+              description: "Pay the bills",
+              priority: true,
+              done: false,
+            },
+            {
+              id: 3,
+              description: "Buy some computer games",
+              priority: false,
+              done: false,
+            },
+            {
+              id: 4,
+              description: "Go to the gym",
+              priority: false,
+              done: false,
+            },
+          ],
+        },
+        {
+          id: 2,
+          name: "Life Project",
+          tasks: [
+            {
+              id: 1,
+              description: "Visit parents",
+              priority: false,
+              done: false,
+            },
+            {
+              id: 2,
+              description: "Visit uncles",
+              priority: true,
+              done: false,
+            },
+            {
+              id: 3,
+              description: "Go around the world",
+              priority: false,
+              done: false,
+            },
+            {
+              id: 4,
+              description: "Quit smoking",
+              priority: false,
+              done: false,
+            },
+          ],
         },
       ],
     };
